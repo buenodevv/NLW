4882067a38c3bd9fd7f388a530c5bbcfa1728d1a
buenodevv <64937739+buenodevv@users.noreply.github.com>
2020-06-06 14:33:42 -0300

    Add files via upload

---
+++ public/scrips/intro.js
@@ -0,0 +1,38 @@
+//Objeto possuem propriedades e funcionalidade
+//const pessoa = {
+  //altura: "1,80m",
+  //idade: 26,
+  //solteira: true,
+  //correr(){
+   // return "run Forest"
+  //}
+// }
+
+//document.write(pessoa.correr())
+
+//array - vetores
+//const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+//document.write(colecaoDeBolinhas[3].name)
+
+//Registrar Funcao
+//function sayMyName(name){
+  
+ // document.write(name)
+//}
+
+// executar
+//reuso de codigo
+//sayMyName("Lucas")
+
+//const notaFinal = 4
+
+//if(notaFinal < 5 ){
+ // document.write("Vc foi reprovado")
+//}else{
+ // document.write("Vc foi aprovado")
+//}
+
+//for (i=0; i < 10; i++){
+ // document.write(`<p>${i}</p>`)
+//}
